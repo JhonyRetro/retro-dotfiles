58446c9cf59a623aa03d47a60b438d5f19c562fc
JhonyRetro <jhonan989@gmail.com>
2026-08-19 02:53:01 +0200

    bruh 2: electric boogaloo

--- scripts/config_setup.sh
+++ scripts/config_setup.sh
@@ -8,7 +8,7 @@ if ! command -v stow &> /dev/null; then
     exit 1
 fi
 
-DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DOTFILES_DIR="$(cd "../$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [ ! -d "$DOTFILES_DIR/configs" ]; then
     echo "Error: 'configs' folder not found in $DOTFILES_DIR. Make sure to clone the repository cleanly."
